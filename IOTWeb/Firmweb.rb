1ec2b495ca7ea0592072f64db1001efd8386458f
Stephen Daniel <swd@pobox.com>
2017-02-25 09:04:36 -0500

    Added some basic error handling and pathname escaping to the firmware handler

--- IOTWeb/Firmweb.rb
+++ IOTWeb/Firmweb.rb
@@ -9,6 +9,21 @@ set :bind, '0.0.0.0'
 # names of the fields in an OTA request
 req_field_names = %w{ device firmware version_current version_desired }
 
+# Used for handling errors during OTA processing
+class OtaError < Exception
+  def initialize(msg)
+    @msg = msg
+  end
+
+  def to_S
+    @msg
+  end
+end
+
+
+# Routes go here
+
+
 get '/' do
 	logger.info("route /")
 	"Hello World!"
@@ -17,15 +32,24 @@ end
 get '/custom_ota' do
 	logger.info("route custom ota")
 
+    begin
 	# First, set up a hash, reqfields, indexed by the field names (above)
-	tmp = Array.new
-	request.env['HTTP_X_ESP8266_VERSION'].each_line('=') {|f| tmp << f.chomp('=')}
 	reqfields = Hash.new
-	tmp.each_index {|i| reqfields[req_field_names[i]] = tmp[i]}
+	begin
+	    tmp = Array.new
+	    request.env['HTTP_X_ESP8266_VERSION'].each_line('=') {|f| tmp << CGI.escape(f.chomp('='))}
+	    tmp.each_index {|i| reqfields[req_field_names[i]] = tmp[i]}
+	rescue Exception
+	    raise OtaError.new("Missing or unparsable HTTP_X_ESP8266_VERSION header")
+	end
 
 	# Now, find the firmware file
-	#xxx firmware_file = 'FIRMWARE/hello-world/0.2.2'
 	firmware_file = "FIRMWARE/#{reqfields['firmware']}/#{reqfields['version_desired']}"
+	raise OtaError.new("Requested file #{firmware_file} does not exist") unless File.exist?(firmware_file)
 	logger.info("Sending file " + firmware_file)
 	#send_file firmware_file
+    
+    rescue OtaError
+    	logger.info("OTA Error: #{$!.to_S}");
+    end
 end
