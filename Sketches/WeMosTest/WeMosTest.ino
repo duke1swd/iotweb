553b7090182f8f40710bf16a21b993e84759e949
duke1swd <swd@pobox.com>
2018-05-19 16:20:16 -0400

    Putting the IOT device firmware files in github

---
+++ Sketches/WeMosTest/WeMosTest.ino
@@ -0,0 +1,21 @@
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(D4, OUTPUT);
+  pinMode(D8, OUTPUT);
+  pinMode(D3, OUTPUT);
+  pinMode(D0, INPUT_PULLUP);
+  pinMode(D1, INPUT_PULLUP);
+}
+
+int v;
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  v = (millis()/100) & 1;
+  digitalWrite(D4, v); // on board LED
+  v = (millis()/200) & 1;
+  digitalWrite(D8, v);
+  v = (millis()/400) & 1;
+  digitalWrite(D3, v);
+}
