728e967539d22fb273e09366433266b54204b2bd
duke1swd <swd@pobox.com>
2020-02-21 16:59:23 -0500

    Fixed.

--- Sketches/WeMosTest/WeMosTest.ino
+++ Sketches/WeMosTest/WeMosTest.ino
@@ -1,21 +1,15 @@
+#include <ESP8266WiFi.h>
+
+#define LED LED_BUILTIN // Note, LED goes on when this pin driven low.
+#define LEDHZ 100
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(D4, OUTPUT);
-  pinMode(D8, OUTPUT);
-  pinMode(D3, OUTPUT);
-  pinMode(D0, INPUT_PULLUP);
-  pinMode(D1, INPUT_PULLUP);
+  pinMode(LED, OUTPUT);
 }
 
-int v;
-
 void loop() {
-  // put your main code here, to run repeatedly:
-  v = (millis()/100) & 1;
-  digitalWrite(D4, v); // on board LED
-  v = (millis()/200) & 1;
-  digitalWrite(D8, v);
-  v = (millis()/400) & 1;
-  digitalWrite(D3, v);
+  if (millis() / (LEDHZ / 2) % 2 == 1)
+    digitalWrite(LED, LOW);
+  else
+    digitalWrite(LED, HIGH);
 }
