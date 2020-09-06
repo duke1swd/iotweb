b0d7bd86f831585a87a63399e216cf100662cabe
duke1swd <swd@pobox.com>
2020-09-06 09:33:20 -0400

    First project, first commit, using platformio. I've created a simple Homie3 project, compiled it, loaded it, and got it running. The config.json file is missing the password, for obvious reasons.

---
+++ projects/Homie 3 Basic/src/main.cpp
@@ -0,0 +1,67 @@
+#include <Arduino.h>
+/*
+ * Modified from Homie 2.0 documentation
+ *
+ * Use as a test case to prove that the setProperty method does
+ * not call the handler for that property.  But it does send
+ * out the MQTT message with the property's new value.
+ */
+#include <Homie.h>
+
+#define FIRMWARE_NAME     "led-control-Wemos"
+#define FIRMWARE_VERSION  "0.1.2"
+
+/*
+ * IO Pins
+ */
+const int PIN_LED = 2;
+
+/*
+ * Global State
+ */
+bool on;
+
+HomieNode ledNode("led", "simpleLedControl", "switch");
+
+bool ledOnHandler(const HomieRange& range, const String& value) {
+  if (value != "true" && value != "false") return false;
+
+  on = (value == "true");
+  digitalWrite(PIN_LED, on ? HIGH : LOW);
+  ledNode.setProperty("on").send(value);
+  Homie.getLogger() << "LED is " << (on ? "on" : "off") << endl;
+
+  return true;
+}
+
+/*
+ * This code called once to set up, but only after completely connected.
+ */
+void setupHandler() {
+}
+
+/*
+ * This code is called once per loop(), but only
+ * when connected to WiFi and MQTT broker
+ */
+void loopHandler() {
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial << endl << endl;
+  pinMode(PIN_LED, OUTPUT);
+  digitalWrite(PIN_LED, LOW);
+
+  Homie_setFirmware(FIRMWARE_NAME, FIRMWARE_VERSION);
+
+  ledNode.advertise("on").settable(ledOnHandler);
+
+  Homie.setSetupFunction(setupHandler).setLoopFunction(loopHandler);
+
+  Homie.setup();
+}
+
+void loop() {
+  Homie.loop();
+}
