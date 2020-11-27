9df24d441f70194c8576d5ebf77c49c013fb28c3
Stephen Daniel <swd@pobox.com>
2020-11-27 08:41:44 -0500

    Augment script to pull the live password

--- config_init.sh
+++ config_init.sh
@@ -1,1 +1,13 @@
-curl -X PUT http://192.168.123.1/config --header "Content-Type: application/json" -d @config.json
+#!/bin/bash
+function on_exit {
+	rm -f /tmp/config.json
+}
+
+trap on_exit exit
+
+# Insert the password into config.json
+PASS=`tail -1 /usr/local/credentials/DanielIOT.txt`
+sed < config.json > /tmp/config.json -e s/PASSWORD/$PASS/
+
+# Config the device!
+curl -X PUT http://192.168.123.1/config --header "Content-Type: application/json" -d @/tmp/config.json
