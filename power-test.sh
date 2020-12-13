783abb994048efc4af6f533d227e26f041760f72
Stephen Daniel <swd@pobox.com>
2020-12-13 17:26:02 -0500

    Minor tweaks to some shell scripts

--- power-test.sh
+++ power-test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-mosquitto_pub -r -t 'devices/plug-0001/outlet/on/set' -m 'true'
+mosquitto_pub -r -t 'devices/plug-0002/outlet/on/set' -m 'true'
 sleep 1
-mosquitto_pub -r -t 'devices/plug-0001/outlet/on/set' -m 'false'
-mosquitto_pub -r -t 'devices/plug-0001/button/button/set' -m 'false'
+mosquitto_pub -r -t 'devices/plug-0002/outlet/on/set' -m 'false'
+mosquitto_pub -r -t 'devices/plug-0002/button/button/set' -m 'false'
--- set-on.sh
+++ set-on.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mosquitto_pub -r -t 'devices/led-0001/led/on/set' -m '10'
+mosquitto_pub -r -t 'devices/led-0002/led_0/on/set' -m '10'
